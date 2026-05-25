7453b70de7971940c5206fbe77caeb6ba186b33c
Daniel_Barrios <fp23eleb0139@aulatomillo.org>
2026-05-26 01:36:39 +0200

    Ejercicio 1 Book Terminado

--- src/main/java/es/ing/tomillo/library/model/Book.java
+++ src/main/java/es/ing/tomillo/library/model/Book.java
@@ -2,22 +2,67 @@ package es.ing.tomillo.library.model;
 
 import java.util.Objects;
 
+    //Ej 1
 public class Book {
-    // TODO: Implementar los atributos según el ejercicio 1
-    // - titulo (String)
-    // - autor (String)
-    // - isbn (String)
-    // - disponible (boolean)
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private boolean disponible;
 
-    // TODO: Implementar constructor según el ejercicio 1
-    public Book() {
+    //Ej 2
+    public Book(String titulo, String autor, String isbn) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.disponible = true;
     }
 
-    // TODO: Implementar getters y setters según el ejercicio 1
+    //Ej 3
+    public String getTitle() {
+        return titulo;
+    }
 
-    // TODO: Implementar método toString según el ejercicio 1
+    public void setTitle(String titulo) {
+        this.titulo = titulo;
+    }
 
-    // TODO: Implementar método equals para comparar libros por ISBN
+    public String getAuthor() {
+        return autor;
+    }
 
+    public void setAuthor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isAvailable() {
+        return disponible;
+    }
+
+    public void setAvailable(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    //Ej 4
+    @Override
+    public String toString() {
+        return titulo + " - " + autor + " (ISBN: " + isbn + ")";
+    }
+
+    //Ej 5
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(isbn, book.isbn);
+    }
 }
 
